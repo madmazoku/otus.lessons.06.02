31de8cc44424d62fb7c878dcbdda94354be836ab
madmazoku <madmazoku@gmail.com>
2017-12-29 14:28:16 +0300

    fix lesson number

--- docker/docker.sh
+++ docker/docker.sh
@@ -5,4 +5,4 @@ docker run --rm -i otus.lessons.06.02 allocator -v
 echo ========= Allocate, result will go to ../bin/out.txt
 docker run --rm -i otus.lessons.06.02 otus.lessons.06.02
 echo ========= Remove docker image
-docker rmi otus.lessons.06.01
+docker rmi otus.lessons.06.02
